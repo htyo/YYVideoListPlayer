f3279c00231bf4c8e0cc57ed681cfbb2e1fd7705
yanbao <yanbao@yanbaodeMac-mini.local>
2026-04-11 14:16:55 +0800

    version:1.0.2,build:10203

--- scripts/cocoapods_destination_patch.rb
+++ scripts/cocoapods_destination_patch.rb
@@ -1,4 +1,5 @@
 require 'fourflusher'
+require 'cocoapods'
 
 module Fourflusher
   class SimControl
@@ -10,6 +11,28 @@ module Fourflusher
         return ['-destination', dest] if dest && !dest.empty?
         _codex_orig_destination(filter, os, minimum_version)
       end
+    end
+  end
+end
+
+class Pod::Validator
+  unless method_defined?(:_codex_orig_download_pod)
+    alias_method :_codex_orig_download_pod, :download_pod
+
+    def download_pod
+      _codex_orig_download_pod
+      yy_fix_zfplayer_private_header!
+    end
+
+    private
+
+    def yy_fix_zfplayer_private_header!
+      file = validation_dir + 'Pods/ZFPlayer/ZFPlayer/Classes/Core/ZFReachabilityManager.m'
+      return unless file.exist?
+
+      content = file.read
+      fixed = content.gsub('<netinet6/in6.h>', '<netinet/in.h>')
+      file.open('w') { |f| f.write(fixed) } if fixed != content
     end
   end
 end
