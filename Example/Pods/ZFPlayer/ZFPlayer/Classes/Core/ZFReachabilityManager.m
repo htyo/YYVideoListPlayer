8d29136e85cd72258c5e78849270edd4520bece5
yanbao <yanbao@yanbaodeMac-mini.local>
2026-04-11 14:00:29 +0800

    version:1.0.2,build:10203

--- Example/Pods/ZFPlayer/ZFPlayer/Classes/Core/ZFReachabilityManager.m
+++ Example/Pods/ZFPlayer/ZFPlayer/Classes/Core/ZFReachabilityManager.m
@@ -25,6 +25,8 @@
 #import "ZFReachabilityManager.h"
 #if !TARGET_OS_WATCH
 #import <netinet/in.h>
+#import <netinet6/in6.h>
+#import <arpa/inet.h>
 #import <ifaddrs.h>
 #import <netdb.h>
 #import <CoreTelephony/CTTelephonyNetworkInfo.h>
