3d80b565438460b760ca538479a7ba3cc68dd950
yanbao <yanbao@yanbaodeMac-mini.local>
2026-04-13 14:53:07 +0800

    Version:1.0.4,build:10401

--- YYVideoListPlayer/Classes/Controllers/YYVideoListPlayerController.m
+++ YYVideoListPlayer/Classes/Controllers/YYVideoListPlayerController.m
@@ -268,7 +268,7 @@
     self.currentIndexPath = indexPath;
     [self didChangeIndexPathForVisible];
 
-    dispatch_async(dispatch_get_main_queue(), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.25 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         self.currentDisplayCell = (YYVideoListPlayerCell *)[self.listPlayerView cellForItemAtIndexPath:indexPath];
         [self play];
         if (completion) completion();
